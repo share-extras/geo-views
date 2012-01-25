2682fb7054db2f6ada288865de7997be8a592a79
Will Abson <will.abson@gmail.com>
2012-01-25 16:49:58 +0000

    Document Geographic Details should now uses Leaflet for OSM view, and added configuration options to component
    git-svn-id: https://share-extras.googlecode.com/svn/trunk/Document Geographic Details@909 a3f5c567-fd0f-3a89-9b71-a290c5a5f590

--- web/extras/components/geographic/osm-map.js
+++ web/extras/components/geographic/osm-map.js
@@ -68,8 +68,40 @@ if (typeof Extras.component == "undefined" || !Extras.component)
           *
           * @property documentDetails
           * @type object
+          * @default null
           */
-         documentDetails: null
+         documentDetails: null,
+
+         /**
+          * Map type to display, either "leaflet" or "openlayers"
+          *
+          * @property mapType
+          * @type string
+          * @default "leaflet"
+          */
+         mapType: "leaflet",
+         
+         /**
+          * URL with parameters for fetching map tiles
+          * 
+          * @property leafletTileUrl
+          * @type string
+          */
+         leafletTileUrl: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+         
+         /**
+          * Default map zoom
+          * @type int
+          * @default 15
+          */
+         mapInitialZoom: 15,
+
+         /**
+          * Whether to display the location retrieved from from reverse geocode service
+          * @type boolean
+          * @default true
+          */
+         loadLocation: true
       },
 
       /**
@@ -98,86 +130,117 @@ if (typeof Extras.component == "undefined" || !Extras.component)
          // Get node's geo location info
          var properties = this.recordData.node.properties;
 
-         var map = new OpenLayers.Map(this.id + "-map", {
-                 //controls:[
-                 //    new OpenLayers.Control.PanZoomBar(),
-                 //    new OpenLayers.Control.Attribution()
-                 //],
-                 projection: new OpenLayers.Projection("EPSG:900913"),
-                 displayProjection: new OpenLayers.Projection("EPSG:4326")
-             }),
-             layer = new OpenLayers.Layer.OSM("OSM Map"),
-             latlng = new OpenLayers.LonLat(properties["cm:longitude"], properties["cm:latitude"]),
-             zoom = 15;
-         
-         map.addLayer(layer);
-         map.addControl(new OpenLayers.Control.PanZoomBar());
-         map.setCenter(latlng.transform(map.displayProjection, map.projection), zoom);
-         
-         // Pan the map before adding the marker
-         map.pan(0, -100, {
-             animate: false
-         });
-         
-         // Add the marker
-         var markers = new OpenLayers.Layer.Markers("Markers");
-         map.addLayer(markers);
-         
-         autoSizeAnchoredBubble = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {
-             'autoSize': true,
-             'maxSize': new OpenLayers.Size(450, 450)
-         });
-         
-         var feature = new OpenLayers.Feature(markers, latlng); 
-         feature.closeBox = true;
-         feature.popupClass = autoSizeAnchoredBubble;
-         feature.data.popupContentHTML = Dom.get(this.id + "-info").innerHTML;
-         feature.data.overflow = "auto";
-         
-         var marker = feature.createMarker();
-
-         var markerClick = function (evt) {
-             if (this.popup == null) {
-                 this.popup = this.createPopup(this.closeBox);
-                 map.addPopup(this.popup);
-                 this.popup.show();
-             } else {
-                 this.popup.toggle();
-             }
-             currentPopup = this.popup;
-             if (evt != null)
-             {
-                 OpenLayers.Event.stop(evt);
-             }
-         };
-         marker.events.register("mousedown", feature, markerClick);
-         markers.addMarker(marker);
-         
-         // Fake marker click
-         markerClick.call(feature, null);
-         
-         // Use Ajax to load location info
-         Alfresco.util.Ajax.request(
+         if (this.options.mapType == "openlayers" && typeof OpenLayers == "object")
          {
-            url: Alfresco.constants.PROXY_URI.replace("/alfresco/", "/osm-nominatim/") + "reverse?format=json&lat=" + properties["cm:latitude"] + "&lon=" + properties["cm:longitude"] + "&zoom=12",
-            successCallback:
-            {
-               fn: function OSMMap_Nominatim_SC(resp) {
-                   if (typeof resp.json == "object")
-                   {
-                       Dom.get(this.id + "-location").innerHTML = this.msg("label.locationName", resp.json.display_name);
-                   }
-               },
-               scope: this
-            },
-            failureCallback:
-            {
-               fn: this.onPollLoadFailed,
-               scope: this
-            },
-            scope: this,
-            noReloadOnAuthFailure: true
-         });
+             var map = new OpenLayers.Map(this.id + "-map", {
+                     //controls:[
+                     //    new OpenLayers.Control.PanZoomBar(),
+                     //    new OpenLayers.Control.Attribution()
+                     //],
+                     projection: new OpenLayers.Projection("EPSG:900913"),
+                     displayProjection: new OpenLayers.Projection("EPSG:4326")
+                 }),
+                 layer = new OpenLayers.Layer.OSM("OSM Map"),
+                 latlng = new OpenLayers.LonLat(properties["cm:longitude"], properties["cm:latitude"]),
+                 zoom = this.options.mapInitialZoom;
+             
+             map.addLayer(layer);
+             map.addControl(new OpenLayers.Control.PanZoomBar());
+             map.setCenter(latlng.transform(map.displayProjection, map.projection), zoom);
+             
+             // Pan the map before adding the marker
+             map.pan(0, -100, {
+                 animate: false
+             });
+             
+             // Add the marker
+             var markers = new OpenLayers.Layer.Markers("Markers");
+             map.addLayer(markers);
+             
+             autoSizeAnchoredBubble = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {
+                 'autoSize': true,
+                 'maxSize': new OpenLayers.Size(450, 450)
+             });
+             
+             var feature = new OpenLayers.Feature(markers, latlng); 
+             feature.closeBox = true;
+             feature.popupClass = autoSizeAnchoredBubble;
+             feature.data.popupContentHTML = Dom.get(this.id + "-info").innerHTML;
+             feature.data.overflow = "auto";
+             
+             var marker = feature.createMarker();
+
+             var markerClick = function (evt) {
+                 if (this.popup == null) {
+                     this.popup = this.createPopup(this.closeBox);
+                     map.addPopup(this.popup);
+                     this.popup.show();
+                 } else {
+                     this.popup.toggle();
+                 }
+                 currentPopup = this.popup;
+                 if (evt != null)
+                 {
+                     OpenLayers.Event.stop(evt);
+                 }
+             };
+             marker.events.register("mousedown", feature, markerClick);
+             markers.addMarker(marker);
+             
+             // Fake marker click
+             markerClick.call(feature, null);
+         }
+         else if (this.options.mapType == "leaflet" && typeof L == "object")
+         {
+             // set up the map
+             var map = new L.Map(this.id + "-map");
+
+             // create the tile layer with correct attribution
+             var osm = new L.TileLayer(this.options.leafletTileUrl, {attribution: this.msg("label.copyright.osm")});
+             var latlng = new L.LatLng(properties["cm:latitude"], properties["cm:longitude"]);
+
+             // start the map centred on the icon
+             map.setView(latlng, this.options.mapInitialZoom);
+             map.addLayer(osm);
+             
+             // create a marker in the given location and add it to the map
+             var marker = new L.Marker(latlng);
+             map.addLayer(marker);
+
+             // attach a given HTML content to the marker and immediately open it
+             marker.bindPopup(Dom.get(this.id + "-info").innerHTML, {}).openPopup();
+         }
+         else
+         {
+             Dom.get(this.id + "-map").innerHTML = this.msg("error.badMapType");
+         }
+         
+         // Load location information from Nominatim, if enabled
+         if (this.options.loadLocation)
+         {
+             // Use Ajax to load location info
+             Alfresco.util.Ajax.request(
+             {
+                url: Alfresco.constants.PROXY_URI.replace("/alfresco/", "/osm-nominatim/") + "reverse?format=json&lat=" + properties["cm:latitude"] + "&lon=" + properties["cm:longitude"] + "&zoom=12",
+                successCallback:
+                {
+                   fn: function OSMMap_Nominatim_SC(resp) {
+                       if (typeof resp.json == "object")
+                       {
+                           Dom.get(this.id + "-location").innerHTML = this.msg("label.locationName", resp.json.display_name);
+                       }
+                   },
+                   scope: this
+                },
+                failureCallback:
+                {
+                   fn: this.onPollLoadFailed,
+                   scope: this
+                },
+                scope: this,
+                noReloadOnAuthFailure: true
+             });
+         }
       },
 
       /**
